7a2e9b1ad51a21c244d29042c075ec5d120980b9
nathaniel tse <nathanieltse@hotmail.com>
2021-05-02 21:35:18 -0400

    fixed decimal place

--- budget/models.py
+++ budget/models.py
@@ -17,7 +17,7 @@ class Spending(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     item = models.CharField(max_length=100, null=True)
-    item_price = models.DecimalField(max_digits=10, decimal_places=0)
+    item_price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
 
     def __str__(self):
@@ -44,7 +44,7 @@ class Budget(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE)
     timestamp = models.DateField(auto_now_add=False)
     item = models.CharField(max_length=100, null=True)
-    item_price = models.DecimalField(max_digits=10, decimal_places=0)
+    item_price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
 
     def __str__(self):
